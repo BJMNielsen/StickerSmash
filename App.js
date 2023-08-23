d205dc37cb8f07bb4c4d02edd3fe919e9d78dace
benj302g <benj302g@stud.kea.dk>
2023-08-23 12:43:10 +0200

    -init

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Image source={{uri: 'https://mb.cision.com/Public/MigratedWpy/91322/745520/a8bbb399629629fc_800x800ar.jpg'}}
+       style={{width: 300, height: 200, marginBottom: 50}} />
+      <Text>Tryg forsikring</Text>
+      <Text>Benjamin Joshua Munck Nielsen</Text>
+      <Button title='Hyr mig' style={{}}>Hej</Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
